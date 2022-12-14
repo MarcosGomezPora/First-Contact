76a5d906490c790caff0ddbd234f1d03d43a51ea
MarcosGomezPora <marcosgomezpora@gmail.com>
2022-12-14 22:39:25 +0100

    Iteration#4

--- upgrade-basic-algorithms.js
+++ upgrade-basic-algorithms.js
@@ -30,16 +30,55 @@ car2.finalPrice = globalBasePrice + car2.basePrice;
 console.log(car1);
 
 // Iteración 3
+//Iteración 3 1.1
 const multi = 10 * 5;
 console.log(multi);
+
+//Iteración 3 1.2
 const divi = 10 / 2;
 console.log(divi);
+
+//Iteración 3 1.3
 const subt = 15 - 9;
 console.log(subt);
+
+//Iteración 3 1.4
 let y = 10;
 let z = 5;
 let x = y + z;
 console.log(x);
+
+//Iteración 3 1.5
 let x = y * z;
 console.log(x);
+
+//Iteración 4
+//Iteración 4 1.1
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+//Iteración 4 1.2
+avengers [0] = "IRON MAN";
+console.log(avengers);
+
+//Iteración 4 1.3
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers.length);
+
+//Iteración 4 1.4
+const RickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+RickAndMortyCharacters.push("Morty", "Summer");
+console.log(RickAndMortyCharacters[RickAndMortyCharacters.length - 1]);
+
+//Iteración 4 1.5
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters[0], rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+//Iteración 4 1.6
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.splice(1,1);
+console.log(rickAndMortyCharacters);
+
+
 
