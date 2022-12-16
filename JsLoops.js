a7d2d7212dd566abd58a3b67b31c60b4854cc708
MarcosGomezPora <marcosgomezpora@gmail.com>
2022-12-16 15:00:03 +0100

    Iteration5Loops

--- JsLoops.js
+++ JsLoops.js
@@ -44,3 +44,14 @@ const alien = {
 for (const key in alien) {
     console.log(alien[key]);
     }
+
+//Iteración5
+const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+for (let i = 0; i < placesToTravel.length; i++) {
+    const element = placesToTravel[i];
+    if (element.id === 11 || element.id ===40) {
+        placesToTravel.splice(i, 1);
+    }
+    
+}
+console.log(placesToTravel);
