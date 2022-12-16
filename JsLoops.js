4e5dce5ee832b431f592797c00fc3452964658e0
MarcosGomezPora <marcosgomezpora@gmail.com>
2022-12-16 14:31:07 +0100

    Iteration2Loops

--- JsLoops.js
+++ JsLoops.js
@@ -7,3 +7,24 @@ for (const product of products) {
         console.log(product)
     }
 }
+
+//Iteración 2
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+	{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+	{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+	{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+	{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+//
+for (const alumn of alumns) {
+   let T1 = alumn.T1
+   let T2 = alumn.T2
+   let T3 = alumn.T3
+   if (T1 && T2 || T2 & T3 || T1 && T3) {
+        alumn.isAproved = true
+   } else {
+    alumn.isAproved = false
+   }
+}
+console.log(alumns);
