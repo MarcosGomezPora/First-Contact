e9c96702a6453cb970bdf738cbb2a457f319b07b
MarcosGomezPora <marcosgomezpora@gmail.com>
2022-12-18 22:24:55 +0100

    Iteration3Functions

--- JsFunctions.js
+++ JsFunctions.js
@@ -6,3 +6,20 @@ function sum(numberOne , numberTwo) {
         return numberTwo;
     }
   }
+
+//Iteración2
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+    function findLongestWord(param) {
+}
+
+//Iteración3
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(array) {
+    let sumTot = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumTot += array[i];
+    }
+    return console.log(sumTot);
+}
+sumAll(numbers);
