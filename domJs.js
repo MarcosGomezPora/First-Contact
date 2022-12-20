a965a5bf0bff7093347a759a8ee640b30dae049a
MarcosGomezPora <119228424+MarcosGomezPora@users.noreply.github.com>
2022-12-20 09:54:45 +0100

    Add files via upload

---
+++ domJs.js
@@ -0,0 +1,22 @@
+//Iteración1
+
+let classShowme = document.querySelector(".showme");
+console.log(classShowme);
+
+let classIdPillado = document.querySelector("#pillado");
+console.log(classIdPillado);
+
+let allP = document.querySelectorAll("p");
+console.log(allP);
+
+let classPokemon = document.querySelectorAll(".pokemon");
+console.log(classPokemon);
+
+let data = document.querySelectorAll('[data-function="testMe"]');
+console.log(atrTest);
+
+
+
+
+
+
